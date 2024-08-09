3499d5f7b3b437e2e4256bfd7f083b2058164b31
PranabKumar27 <pranabkumarsahoo27@gmail.com>
2024-08-09 21:16:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wheather from './Components/Weather'
+
+function App() {
+  return (
+    <div className='app'>
+      <Wheather/>
+      
+    </div>
+  )
+}
+
+export default App
